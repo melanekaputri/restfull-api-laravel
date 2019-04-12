4c6d869a0bd52ce06b1bb563a8015c082066dc2c
Melan Eka Putri <melanputrimelan@gmail.com>
2019-04-12 15:20:02 +0700

    5. User Transformer menggunakan Fractal

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Transformers\UserTransformer;
 
 class UserController extends Controller
 {
     public function users(User $user)
     {
         $users = $user->all();
-        return response()->json($users);
+        
+        return fractal()
+            ->collection($users)
+            ->transformWith(new UserTransformer)
+            ->toArray();
     }
 }
