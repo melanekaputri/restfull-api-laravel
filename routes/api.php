919b67cd3fa2a4af4e821fe40461ceeca06b4275
Melan Eka Putri <melanputrimelan@gmail.com>
2019-04-12 17:10:43 +0700

    7. Login User

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Http\Request;
 
 Route::get('users','UserController@users');
 Route::post('auth/register','AuthController@register');
+Route::post('auth/login','AuthController@login');
